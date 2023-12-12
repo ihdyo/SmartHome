b2a870fd94ddbd581dc0f32d0dbebdfa63efe76c
ihdyo <yodhianugrah23@gmail.com>
2023-12-12 12:19:10 +0700

    TODO: Fetch Data

--- app/src/main/java/com/ihdyo/smarthome/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ihdyo/smarthome/ui/home/HomeFragment.kt
@@ -118,6 +118,10 @@ class HomeFragment : Fragment() {
         return root
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
